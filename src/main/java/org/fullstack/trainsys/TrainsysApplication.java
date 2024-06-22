3e9cf269f0bfd46235fb07a64e06d366f27ddeae
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-06-22 19:32:15 -0300

    initial commit

---
+++ src/main/java/org/fullstack/trainsys/TrainsysApplication.java
@@ -0,0 +1,13 @@
+package org.fullstack.trainsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fullstack/trainsys/TrainsysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fullstack.trainsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
