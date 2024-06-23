86d339e67177927f7b53876d46c56adb6218b8fd
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-06-23 17:52:38 -0300

    criada entity workout

---
+++ src/main/java/org/fullstack/trainsys/entity/WorkoutEntity.java
@@ -0,0 +1,39 @@
+package org.fullstack.trainsys.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "workouts")
+public class WorkoutEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinColumn(name = "student_id", referencedColumnName = "id", nullable = false)
+    private StudentEntity student;
+
+    @Column(name = "exercise_id", nullable = false)
+    private ExerciseEntity exercise;
+
+    @Column(name = "repetitions", nullable = false)
+    private Integer repetitions;
+
+    @Column(name = "weight", nullable = false)
+    private Float weight;
+
+    @Column(name = "break_time", nullable = false)
+    private Integer break_time;
+
+    @Enumerated(EnumType.STRING)
+    private WeekDay day;
+
+    @Column(name = "observations")
+    private String observations;
+
+    @Column(name = "time", nullable = false)
+    private Integer time;
+}
