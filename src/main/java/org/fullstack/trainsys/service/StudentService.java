adc669aa0740118535b94c886436aa943792a043
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-06-24 19:51:00 -0300

    ajustado service e serviceimpl student

--- src/main/java/org/fullstack/trainsys/service/StudentService.java
+++ src/main/java/org/fullstack/trainsys/service/StudentService.java
@@ -3,10 +3,12 @@ package org.fullstack.trainsys.service;
 import org.fullstack.trainsys.entity.StudentEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
     StudentEntity createStudent(StudentEntity student);
     List<StudentEntity> readStudent();
+    Optional<StudentEntity> readStudentById(Long id);
     StudentEntity updateStudent(Long id, StudentEntity updatedStudent);
     void deleteStudent(Long id);
 }
