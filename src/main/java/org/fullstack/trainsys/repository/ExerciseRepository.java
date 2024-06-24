f87dd2d97955617b953815bdb0301ad10c105510
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-06-24 00:33:45 -0300

    criado repository exercise

---
+++ src/main/java/org/fullstack/trainsys/repository/ExerciseRepository.java
@@ -0,0 +1,9 @@
+package org.fullstack.trainsys.repository;
+
+import org.fullstack.trainsys.entity.ExerciseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<ExerciseEntity, Long> {
+}
