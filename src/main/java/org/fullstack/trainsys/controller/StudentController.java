5bbdadd9b9a46b767c93a8ca02204a979bd36814
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-06-24 18:48:33 -0300

    criado endpoint student create

---
+++ src/main/java/org/fullstack/trainsys/controller/StudentController.java
@@ -0,0 +1,33 @@
+package org.fullstack.trainsys.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fullstack.trainsys.entity.ExerciseEntity;
+import org.fullstack.trainsys.entity.StudentEntity;
+import org.fullstack.trainsys.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("students")
+public class StudentController {
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    public ResponseEntity<StudentEntity> createStudent(@RequestBody StudentEntity student){
+        StudentEntity newStudent = studentService.createStudent(student);
+        logger.debug("Novo estudante adicionado: {}", newStudent);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newStudent);
+    }
+
+}
