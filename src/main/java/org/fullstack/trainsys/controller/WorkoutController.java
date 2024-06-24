ce176bf2dbbe9747d064512eeb270435a7e8caff
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-06-24 17:27:01 -0300

    nao criado endpoint workout read

--- src/main/java/org/fullstack/trainsys/controller/WorkoutController.java
+++ src/main/java/org/fullstack/trainsys/controller/WorkoutController.java
@@ -7,10 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -30,4 +29,7 @@ public class WorkoutController {
         logger.debug("Novo treino adicionado: {}", newWorkout);
         return ResponseEntity.status(HttpStatus.CREATED).body(newWorkout);
     }
+
+    //@GetMapping("/api/students/{id}/workouts")
+    //public ResponseEntity<List<WorkoutEntity>> readStudentWorkout()
 }
